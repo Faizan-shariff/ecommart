125e73b9cc41aaed46ac2dd04b52d206454403a1
faizan-shariff-tfs <sfaizanshariff22@gmail.com>
2023-10-02 16:27:13 +0530

    Added some components
    added about page, contact page, checkout page, and some incomplete login and register pages

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import Home from "./component/Home";
 import Navbar from "./component/Navbar";
 import { Route, Routes} from "react-router";
 import Products from "./component/Products";
-import { renderMatches } from "react-router-dom";
 import Product from "./component/Product";
 import Cart from "./component/Cart";
+import Checkout from "./component/Checkout";
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Login from "./component/Login";
+import Register from "./component/Register";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route exact path="/login" Component={Login}/>
+        <Route exact path="/register" Component={Register}/>
         <Route exact path="/" Component={Home} />
         <Route exact path="/products" Component={Products} />
         <Route exact path="/products/:id" Component={Product} />
         <Route exact path="/cart" Component={Cart}/>
-
-      </Routes>
+        <Route exact path="/checkout" Component={Checkout}/>
+        <Route exact path="/about" Component={About}/>
+        <Route exact path="/contact" Component={Contact}/>
+        </Routes>
     </>
   );
 }
